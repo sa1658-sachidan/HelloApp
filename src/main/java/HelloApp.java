da050659f47aa30a6630190d80314f7d337e8142
sa1658-sachidan <sa1658@srmist.edu.in>
2026-04-13 15:10:34 +0530

    UC3 is completed

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,12 +1,10 @@
 public class HelloApp {
-
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("No name was provided.");
-        }
+        // Check if arguments are provided
+        String name = (args.length > 0) ? args[0] : "Guest";
 
+        // Print the greeting
+        System.out.println("Hello, " + name + "!");
     }
 }
